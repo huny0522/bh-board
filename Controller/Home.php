fba95b6121382712808b74f4e5e3f3a718e71b15
huny <bhid@naver.com>
2017-02-07 23:24:59 +0900

    fix

--- Controller/Home.php
+++ Controller/Home.php
@@ -6,13 +6,6 @@ class HomeController extends BH_Controller{
 	}
 
 	public function Index(){
-
-		$dbGetList = new BH_DB_GetList(TABLE_POPUP);
-		$dbGetList->AddWhere('begin_date <= \''.date('Y-m-d').'\'');
-		$dbGetList->AddWhere('end_date >= \''.date('Y-m-d').'\'');
-		$dbGetList->AddWhere('enabled=\'y\'');
-		$this->_Value['Popup'] = $dbGetList->GetRows();
-
 		$this->_View();
 	}
 }
