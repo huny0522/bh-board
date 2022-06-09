3287e43124bf0aeef2df3a3928667a5cf6b9800e
bhid <huny0522@gmail.com>
2022-06-09 19:56:54 +0900

    코드용 가이드 추가

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -17,3 +17,38 @@ namespace PHPSTORM_META
 	});
 	expectedArguments(\Common\MenuHelp::SetTitleFunc(), 0, argumentsSet('MenuHelpSetTitleFunc'));
 }
+
+namespace
+{
+	function mv($str){/** @var BH_Model $Model */ echo $Model->data[$str]->Safe();}
+	function mt($str){/** @var BH_Model $Model */ echo $Model->data[$str]->displayName;}
+	function mp($str){/** @var BH_Model $Model */ echo $Model->data[$str]->value;}
+	function mvn($str){/** @var BH_Model $Model */ echo $Model->data[$str]->Num();}
+	function mvr($str){/** @var BH_Model $Model */ echo $Model->data[$str]->SafeRaw();}
+	function mvb($str){/** @var BH_Model $Model */ echo $Model->data[$str]->SafeBr();}
+	function minp($str){/** @var BH_Model $Model */ echo $Model->HTMLPrintInput($str);}
+	function menum($str){/** @var BH_Model $Model */ echo $Model->HTMLPrintEnum($str);}
+
+	define('p', ''); // echo ([code]);
+	define('ifp', ''); // if(isset([code])) echo ([code]);
+
+	define('v', ''); // echo GetDBText([code]);
+	define('ifv', ''); // if(isset([code])) echo GetDBText([code]);
+
+	define('vr', ''); // echo GetDBRaw([code]);
+	define('ifvr', ''); // if(isset([code])) echo GetDBRaw([code]);
+
+	define('vb', ''); // echo nl2br(GetDBText([code]));
+	define('ifvb', ''); // if(isset([code])) echo nl2br(GetDBText([code]));
+
+	define('vstag', ''); // echo strip_tags(GetDBText([code]));
+	define('vbstag', ''); // echo nl2br(GetDBText(strip_tags([code])));
+
+	define('c', ''); // echo BH_Application::URLBase([code]);
+	define('a', ''); // echo BH_Application::URLAction([code]);
+
+	define('fqq', ''); // echo \BH_Application::GetFollowQuery([code], \'?\');
+	define('fqn', ''); // echo \BH_Application::GetFollowQuery([code], \'&\');
+
+	define('inc', ''); // if(_DEVELOPERIS === true || !file_exists(\Paths::DirOfHtml().[code])){require_once \Paths::Dir() . \'/Common/HtmlConvert.php\'; ReplaceHTMLFile(\Paths::DirOfSkin().[code], \Paths::DirOfHtml().[code]);} require \Paths::DirOfHtml().[code];
+}
